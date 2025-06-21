437cf7e57b6a98e102517a163f7ea8f481acb167
Ishita Jain <ishitajain385@gmail.com>
2025-06-21 18:27:57 +0530

    added content

--- src/Components/CleanBeauty.jsx
+++ src/Components/CleanBeauty.jsx
@@ -1,9 +1,13 @@
  import "./CleanBeauty.css"
- 
+ import img from "../assets/cleanbeauty.png"
  export default function CleanBeauty(){
     return(
         <div className="beauty-container">
-        
+        <h2>Clean Beauty. It's in our DNA</h2>
+        <p>Our entire product portfolio is clean, green, and cruelty-free. At least 99% of plant-based ingredients are derived from organic 
+farming, and all products contain a minimum of 98% natural ingredients. We also test our products for suitability to even the most 
+sensitive skin, so the whole family can feel confident using OW.</p>
+<img src={img} alt="" className="imgg" />
         </div>
     )
  }
