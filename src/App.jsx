1955b378d22822f01dab8cee2f81840ba8e7561d
Ishita Jain <ishitajain385@gmail.com>
2025-06-21 18:21:46 +0530

    added few more components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import BathRituals from "./Components/Bath-rituals"
 import ImageSection from "./Components/ImageSection"
 import Video from "./Components/Video"
 import ClinicalApprovals from "./Components/ClinicalApprovals"
+import BestSeller from "./Components/BestSeller"
+import Video2 from "./Components/Video2"
+import CleanBeauty from "./Components/CleanBeauty"
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
    <ImageSection/>
    <Video/>
    <ClinicalApprovals/>
+   <BestSeller/>
+   <Video2/>
+   <CleanBeauty/>
     </>
   )
 }
---
+++ src/Components/CleanBeauty.jsx
@@ -0,0 +1,9 @@
+ import "./CleanBeauty.css"
+ 
+ export default function CleanBeauty(){
+    return(
+        <div className="beauty-container">
+        
+        </div>
+    )
+ }
