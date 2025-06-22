cb1eaab5ba553081b489685424665b72209575e1
Ishita Jain <ishitajain385@gmail.com>
2025-06-22 17:37:44 +0530

    comp added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ClinicalApprovals from "./Components/ClinicalApprovals"
 import BestSeller from "./Components/BestSeller"
 import Video2 from "./Components/Video2"
 import CleanBeauty from "./Components/CleanBeauty"
+import ImageSection2 from "./Components/Image-Section"
+import WhatTheySay from "./Components/WhatTheySay"
 
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
    <BestSeller/>
    <Video2/>
    <CleanBeauty/>
+   <ImageSection2/>
+   <WhatTheySay/>
     </>
   )
 }
