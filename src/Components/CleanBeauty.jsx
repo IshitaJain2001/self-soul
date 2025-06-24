2143e2dde1b3c1b2ac820d3743c5c35b0797f5ee
Ishita Jain <ishitajain385@gmail.com>
2025-06-24 11:54:43 +0530

    rectified css

--- src/Components/CleanBeauty.jsx
+++ src/Components/CleanBeauty.jsx
@@ -8,8 +8,11 @@
         <p>Our entire product portfolio is clean, green, and cruelty-free. At least 99% of plant-based ingredients are derived from organic 
 farming, and all products contain a minimum of 98% natural ingredients. We also test our products for suitability to even the most 
 sensitive skin, so the whole family can feel confident using OW.</p>
+ <div className="imgg" >
+    <img src={img} alt="" />
+    </div>   
  </div>
-<img src={img} alt="" className="imgg" />
+ 
            
        
         </div>
