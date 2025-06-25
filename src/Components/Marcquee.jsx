b18a80c2cc32dfeb7c71b31159b7e66389c5373c
Samarth Jain <samarthj5923@gmail.com>
2025-06-25 22:15:06 +0530

    added marcquee section

--- src/Components/Marcquee.jsx
+++ src/Components/Marcquee.jsx
@@ -4,14 +4,25 @@
  export default function Marcquee(){
     return(
      <div class="marcquee-container">
+      <h3 className="title">@selfsoul_healwithnature</h3>
     <img src={img} alt="Heal with nature banner"/>
-    <div class="features">
-      <p>Organic Ingredients</p>
-      <p><strong>100%</strong> Clinically Tested</p>
-      <p>Suitable For All Skin Types</p>
-      <p><strong>100%</strong> Vegan</p>
-      <p><strong>100%</strong> Cruelty Free</p>
-    </div>
+  <div class="marquee-wrapper">
+  <div class="features">
+    <p>Organic Ingredients</p>
+    <p><strong>100%</strong> Clinically Tested</p>
+    <p>Suitable For All Skin Types</p>
+    <p><strong>100%</strong> Vegan</p>
+    <p><strong>100%</strong> Cruelty Free</p>
+
+
+    <p>Organic Ingredients</p>
+    <p><strong>100%</strong> Clinically Tested</p>
+    <p>Suitable For All Skin Types</p>
+    <p><strong>100%</strong> Vegan</p>
+    <p><strong>100%</strong> Cruelty Free</p>
+  </div>
+</div>
+
   </div>
     )
  }
