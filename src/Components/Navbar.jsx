6ef2935d0ec32cfc8cb96d41993035e7ae38da31
Ishita Jain <ishitajain385@gmail.com>
2025-07-07 13:39:48 +0530

    resolved css issues

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,94 +5,90 @@ import searchIcon from "../assets/search.png";
 import heartIcon from "../assets/heart.png";
 import cartIcon from "../assets/cart.png";
 import userIcon from "../assets/profile.png";
-import img1 from "../assets/summer.png"
-import img2 from "../assets/winter.png"
+import img1 from "../assets/summer.png";
+import img2 from "../assets/winter.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [showBathMenu, setShowBathMenu] = useState(false);
   const [showBodyMenu, setShowBodyMenu] = useState(false);
+
   return (
-    <div className={`drop ${showBathMenu||showBodyMenu ? "active-drop" : ""}`}>
-    <div className="navbar-container">
-    
+    <div className={`drop ${showBathMenu || showBodyMenu ? "active-drop" : ""}`}>
+      <div className="navbar-container">
+        <nav className="navbar">
+          <img src={logo} alt="Logo" className="logo" />
 
- 
-      <nav className="navbar">
-        <img src={logo} alt="Logo" className="logo" />
+          <div className="nav-tab-wrapper">
+            <ul className="nav-links">
+              <li className="active">Home</li>
 
-        <div className="nav-tab-wrapper">
-          <ul className="nav-links">
-            <li className="active">Home</li>
+              {/* BATH DROPDOWN */}
+              <div
+                className="dropdown-wrapper"
+                onMouseEnter={() => setShowBathMenu(true)}
+                onMouseLeave={() => setShowBathMenu(false)}
+              >
+                <li className="dropdown">Bath</li>
 
-         
+                {showBathMenu && (
+                  <div className="dropdown-content">
+                    <ul className="dropdown-left">
+                      <Link to="/Bath-view-all"><li>VIEW ALL</li></Link>
+                      <li>BATH SOAPS</li>
+                      <li>BATH SALTS</li>
+                      <li>BATH OILS</li>
+                      <li>BATH SCRUBS</li>
+                      <li>BATH FIZZ</li>
+                      <li>BATH SOAKS</li>
+                      <li>BATH POWDER</li>
+                    </ul>
+                    <div className="dropdown-img">
+                      <img src={img1} alt="summer" />
+                      <img src={img2} alt="winter" />
+                    </div>
+                  </div>
+                )}
+              </div>
 
-            <li className="dropdown-wrapper"
-  onMouseEnter={() => setShowBathMenu(true)}
-  onMouseLeave={() => setShowBathMenu(false)}
->
-  
-  <div className="dropdown">Bath</div>
-  
-  
-  {showBathMenu && (
-    <div className="dropdown-content">
-      <ul className="dropdown-left">
-     <Link to="Bath-view-all">  <li>VIEW ALL</li> </Link> 
-        <li>BATH SOAPS</li>
-        <li>BATH SALTS</li>
-        <li>BATH OILS</li>
-        <li>BATH SCRUBS</li>
-        <li>BATH FIZZ</li>
-        <li>BATH SOAKS</li>
-        <li>BATH POWDER</li>
-      </ul>
-      <div className="dropdown-img">
-        <img src={img1} alt="" />
-        <img src={img2} alt="" />
+              {/* BODY DROPDOWN */}
+              <div
+                className="dropdown-wrapper"
+                onMouseEnter={() => setShowBodyMenu(true)}
+                onMouseLeave={() => setShowBodyMenu(false)}
+              >
+                <li><Link to="/Body">Body</Link></li>
+
+                {showBodyMenu && (
+                  <div className="dropdown-content positionn">
+                    <ul className="dropdown-left">
+                      <li>Coming Soon</li>
+                    </ul>
+                    <div className="dropdown-img">
+                      <img src={img1} alt="summer" />
+                      <img src={img2} alt="winter" />
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              <li>Face</li>
+              <li>Value Sets</li>
+              <li>Gift</li>
+              <li>Our Heritage</li>
+              <li>Blogs</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+
+          <div className="icons">
+            <img src={searchIcon} alt="Search" />
+            <img src={heartIcon} alt="Wishlist" />
+            <img src={cartIcon} alt="Cart" />
+            <img src={userIcon} alt="User" />
+          </div>
+        </nav>
       </div>
-    </div>
-  )}
-</li>
-
-
-               <li 
-  onMouseEnter={() => setShowBodyMenu(true)}
-  onMouseLeave={() => setShowBodyMenu(false)}
->
-  <Link to="/Body">
-    <div >Body</div>
-  </Link>
-
-  {showBodyMenu && (
-    <div className="dropdown-content positionn">
-      <ul className="dropdown-left">
-    coming soon
-      </ul>
-      <div className="dropdown-img">
-        <img src={img1} alt="" />
-        <img src={img2} alt="" />
-      </div>
-    </div>
-  )}
-</li>
-            <li>Face</li>
-            <li>Value Sets</li>
-            <li>Gift</li>
-            <li>Our Heritage</li>
-            <li>Blogs</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-
-        <div className="icons">
-          <img src={searchIcon} alt="Search" />
-          <img src={heartIcon} alt="Wishlist" />
-          <img src={cartIcon} alt="Cart" />
-          <img src={userIcon} alt="User" />
-        </div>
-      </nav>
-           </div>
     </div>
   );
 };
